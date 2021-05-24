49919642c5108a51c2a7e53738100ea4a458d92e
Jafer Alhaboubi <habboubi@gmail.com>
2021-05-24 13:25:56 -0500

    spring boot application with front-end

--- src/main/java/org/perscholas/controllers/HomeController.java
+++ src/main/java/org/perscholas/controllers/HomeController.java
@@ -1,21 +1,44 @@
 package org.perscholas.controllers;
 
+import lombok.extern.java.Log;
+import org.perscholas.models.Student;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("app")
+@RequestMapping("create")
+@Log
 public class HomeController {
 
-
-    @GetMapping("/test2")
-    public String index(){
-        return "index";
+    @GetMapping("/form")
+    public String form(){
+        return "registerstudent";
     }
 
-    @GetMapping("/test")
-    public String test(){
-        return "hello";
+/*
+    @PostMapping("/newstudent")
+    public String newStudent(@RequestParam("username") String username,
+                             @RequestParam("email") String email,
+                             @RequestParam("password") String password,
+                             Model model
+                             ){
+        Student student = new Student();
+        student.setSUsername(username);
+        student.setSEmail(email);
+        student.setSPassword(password);
+        log.info("Student: " + student);
+        return "student_confirmation";
     }
+
+ */
+
+    @PostMapping("/newstudent")
+    public String newStudent(@ModelAttribute("student") Student student, Model model){
+       log.info("Student: " + student);
+       model.addAttribute("student", student);
+        return "student_confirmation";
+    }
+
+
 }
