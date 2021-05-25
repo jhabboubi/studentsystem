44c9092510a2802c9be4c0b390bf70253ae43cf3
Jafer Alhaboubi <habboubi@gmail.com>
2021-05-25 09:58:59 -0500

    with validation and thymeleaf form conversion

--- src/main/java/org/perscholas/controllers/HomeController.java
+++ src/main/java/org/perscholas/controllers/HomeController.java
@@ -4,11 +4,15 @@ import lombok.extern.java.Log;
 import org.perscholas.models.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("create")
 @Log
+@SessionAttributes({"student"})
 public class HomeController {
 
     @GetMapping("/form")
@@ -33,11 +37,31 @@ public class HomeController {
 
  */
 
+    @ModelAttribute("student")
+    public Student initStudent(){
+        return new Student();
+    }
+
     @PostMapping("/newstudent")
-    public String newStudent(@ModelAttribute("student") Student student, Model model){
-       log.info("Student: " + student);
-       model.addAttribute("student", student);
-        return "student_confirmation";
+    public String newStudent(@ModelAttribute("student") @Valid Student student, BindingResult result,Model model ){
+        System.out.println(result.hasErrors());
+        if(result.hasErrors()) {
+            log.warning("in if statement");
+            log.warning("Error Count: "+String.valueOf(result.getErrorCount()));
+            return "registerstudent";
+
+        }else{
+            log.warning("NOT in if statement");
+            log.info("Student: " + student);
+            model.addAttribute("student", student);
+            return "student_confirmation";
+        }
+    }
+
+    @GetMapping("getsession")
+    public String getSession(){
+
+        return "getsession";
     }
 
 
